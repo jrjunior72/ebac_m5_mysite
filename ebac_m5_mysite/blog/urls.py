db13ad5717c930f2553ecf2f6440cf3aa589099e
jrjunior72 <jrjunior72@hotmail.com>
2025-07-28 23:04:06 -0300

    a rota /admin/ está funcionando

--- ebac_m5_mysite/blog/urls.py
+++ ebac_m5_mysite/blog/urls.py
@@ -1,9 +1,11 @@
+from django.contrib import admin
 from django.urls import path, include
 
 from blog import views
 
 
 urlpatterns = [
+    path('admin/', admin.site.urls),  # <-- ESSA LINHA É ESSENCIAL
     path("home/", views.PostView.as_view(), name="home"),
     path("post/", views.PostView.as_view(), name="post"),
     path("<slug:slug>/", views.PostDetail.as_view(), name="post_detail"),
